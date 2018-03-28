b0a4ea90c32cd03c6d9ec1e025ed5fbfc9dfe073
Trung Le <trunglehai.hvnh@gmail.com>
2018-03-28 05:02:26 +0100

    Revert "Add fminsearchbnd"
    This reverts commit 79c29ccd6d64591aedd97be1c13499d2764c8caa.

--- midasBetaWeights.m
+++
@@ -1,9 +0,0 @@
-function weights = midasBetaWeights(nlag,param1,param2)
-seq = linspace(eps,1-eps,nlag);
-if param1 == 1    
-    weights = (1-seq).^(param2-1);    
-else
-    weights = (1-seq).^(param2-1) .* seq.^(param1-1);    
-end
-weights = weights ./ nansum(weights);
-end
